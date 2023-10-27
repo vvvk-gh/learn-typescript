23df562559677ec1908091ef60b7e6715e0dbc18
vvvk-gh <vamsivutukuri1997@gmail.com>
2023-10-27 16:10:57 +0530

    intial commit : DataTypes in TS

---
+++ README.md
@@ -0,0 +1,13 @@
+CMD TO compile Typescript files
+
+- tsc <filename.ts>
+
+devlopment only package
+
+npm install --save-dev <npm-package-name>
+
+core types in ts
+
+numbers
+string
+boolean
---
+++ app.js
@@ -0,0 +1,9 @@
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+//const n1 = '6'; 
+//Uncommet this to understand the error
+//shows you the lint error as you specified the date type at the function
+const n1 = 6;
+const n2 = 3.9;
+add(n1, n2);
